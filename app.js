26efd173d384fa6ce1bc8cd7aa83df5123ffb764
Yana <kornyanakorny@gmail.com>
2020-11-02 14:02:20 +0300

    Эффективное использование сервера

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const http = require('http');
 const fs = require('fs').promises;
-const hostname = 'localhost';
+const host = 'localhost';
 const port = 1000;
+let indexFile;
 
 const requestListener = function (req, res) {
     fs.readFile(__dirname+'/index.html')
     .then(contents => {
+        indexFile = contents;
         res.setHeader("Content-Type", "text/html");
         res.writeHead(200);
-        res.end(contents);})
+        res.end(indexFile);})
     .catch(err => {
         res.writeHead(500);
         res.end(err);
@@ -17,6 +19,16 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, hostname, () => {
-    console.log(`Server is running on http://${hostname}:${port}`);
-});
+
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}:${port}`);
+        });
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`);
+        process.exit(1);
+    });
+
