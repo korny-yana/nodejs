48636408f394b4cc1163d3b7028279618d5835e9
Yana <kornyanakorny@gmail.com>
2020-11-05 13:52:58 +0300

    Отправляю css файл клиенту, установила pug

--- app.ts
+++ app.ts
@@ -1,51 +1,38 @@
-import { createServer } from 'http'
-import { readFile, access } from 'fs/promises'
-import { readFile as oldReadFile, readdir as oldReadDir,exists, existsSync, readdirSync, lstatSync, truncate, statSync, Dir, opendirSync } from 'fs'
+const http = require('http');
+const fs = require('fs');
 const host = 'localhost';
-const port = 1000;
-let indexFile;
+const port = 9000;
+let htmlFile;
+let cssFile;
 
-const requestListener = function (req, res) {
-    switch (req.url) {
-        case "/": fs.readFile(__dirname+'/index.html')
-    .then(contents => {
-        indexFile = contents;
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end(indexFile);})
-    .catch(err => {
-        res.writeHead(500);
-        res.end(err);
-        console.log("error");
-        return; 
-    });
-    default: 
-    fs.readFile(__dirname+'/style.css')
-    .then(contents => {
-        indexFile = contents;
-        res.setHeader("Content-Type", "text/css");
-        res.writeHead(200);
-        res.end(indexFile);})
-    .catch(err => {
-        res.writeHead(500);
-        res.end(err);
-        console.log("error");
-        return; 
-    });
+fs.readFile('index.html', function(err, data) {
+    if (err){
+        throw err;
     }
-};
+    htmlFile = data;
+});
 
-const server = http.createServer(requestListener);
+fs.readFile('style.css', function(err, data) {
+    if (err){
+        throw err;
+    }
+    cssFile = data;
+});
 
-fs.readFile(__dirname + "/index.html")
-    .then(contents => {
-        indexFile = contents;
-        server.listen(port, host, () => {
-            console.log(`Server is running on http://${host}:${port}`);
-        });
-    })
-    .catch(err => {
-        console.error(`Could not read index.html file: ${err}`);
-        process.exit(1);
-    });
+const server = http.createServer(function (req, res) {
+    switch (req.url) {
+        case "/style.css" :
+            res.writeHead(200, {"Content-Type": "text/css"});
+            res.write(cssFile);
+            break;
+        default :    
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(htmlFile);
+    };
+    res.end();
+});
 
+server.listen(port);
+
+
+console.log(`Server running at http://${host}:${port}`);
