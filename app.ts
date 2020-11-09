9e16421aa967e14687b4280cbb5ab118d65cefb2
Yana <kornyanakorny@gmail.com>
2020-11-09 12:25:21 +0300

    Добавила картинки, использую шаблонизатор pug

--- app.ts
+++ app.ts
@@ -2,16 +2,16 @@ const http = require('http');
 const fs = require('fs');
 const host = 'localhost';
 const port = 9000;
-let htmlFile, secondHtmlFile;
-let cssFile;
+let htmlFile, secondHtmlFile, cssFile, jpgFile, secondJpgFile;
 
-fs.readFile('index.html', function(err, data) {
+
+fs.readFile('page/first_page.html', function(err, data) {
     if (err){
         throw err;
     }
     htmlFile = data;
 });
-fs.readFile('second_page/link.html', function(err, data) {
+fs.readFile('page/second_page.html', function(err, data) {
     if (err){
         throw err;
     }
@@ -24,6 +24,18 @@ fs.readFile('style.css', function(err, data) {
     }
     cssFile = data;
 });
+fs.readFile('img/img1.jpg', function(err, data) {
+    if (err){
+        throw err;
+    }
+    jpgFile = data;
+});
+fs.readFile('img/img2.jpg', function(err, data) {
+    if (err){
+        throw err;
+    }
+    secondJpgFile = data;
+});
 
 const server = http.createServer(function (req, res) {
     switch (req.url) {
@@ -31,9 +43,17 @@ const server = http.createServer(function (req, res) {
             res.writeHead(200, {"Content-Type": "text/css"});
             res.write(cssFile);
             break;
-        case "/second_page/link.html" :
+        case "/second_page.html" :
             res.writeHead(200, {"Content-Type": "text/html"});
             res.write(secondHtmlFile);
+            break;
+        case "/img/img1.jpg" : 
+            res.writeHead(200, {"Content-Type": "image/jpg"});
+            res.write(jpgFile);
+            break;
+        case "/img/img2.jpg" : 
+            res.writeHead(200, {"Content-Type": "image/jpg"});
+            res.write(secondJpgFile);
             break;
         default :    
             res.writeHead(200, {"Content-Type": "text/html"});
