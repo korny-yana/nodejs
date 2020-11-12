f2e9877985452ed5f6c7cd8fe41916554653342f
Yana <kornyanakorny@gmail.com>
2020-11-12 12:15:06 +0300

    заработала отправка файлов клиенту

--- app.ts
+++ app.ts
@@ -4,12 +4,16 @@ const path = require('path');
 const host = 'localhost';
 const port = 9000;
 
-const server = http.createServer (function(req,res)
+functon 
+
+const server = http.createServer (function(req, res)
 {
     let exr = path.extname(req.url);
-    console.log("Hello world");
     let contentType = 'text/html';
     switch (exr) {
+        case '.html':
+            contentType = 'text/html';
+            break;
         case '.js':
           contentType = 'text/javascript';
           break;
@@ -30,44 +34,26 @@ const server = http.createServer (function(req,res)
           break;
     }
     
-    fs.readFile(process.cwd + req.url, function(err, data) {
+    fs.readFile(__dirname + "/"+ req.url, function(err, data) {
     if (err) {
         fs.readFile("error.html", function(err, data) {
             if (err) {
                 throw err
             }
             else{
-                res.writeHead(200, {"Content-Type": "text/html"});
-                        res.write(data);}
+                res.writeHead(200, {"Content-Type": contentType});
+                        res.end(data);
+                    }
               
         })
         // throw err;
     }
     else {
-        switch (req.url) {
-                    // case "/style.css" :
-                    //     res.writeHead(200, {"Content-Type": contentType});
-                    //     res.write(contentType);
-                    //     break;
-                    // case "/link.html" :
-                    //     res.writeHead(200, {"Content-Type": contentType});
-                    //     res.write(contentType);
-                    //     break;
-                    // case "/img/img1.jpg" : 
-                    //     res.writeHead(200, {"Content-Type": contentType});
-                    //     res.write(contentType);
-                    //     break;
-                    // case "/img/img2.jpg" : 
-                    //     res.writeHead(200, {"Content-Type": contentType});
-                    //     res.write(contentType);
-                    //     break;
-                    // default :    
-                    //     res.writeHead(200, {"Content-Type": contentType});
-                    //     res.write(contentType);
-                };
-                res.end();
+        res.writeHead(200, {'Content-Type': contentType})
+        res.end(data, 'utf-8')
+    }
             }
-    });
+    );
 });
 server.listen(port);
 console.log(`Server running at http://${host}:${port}`);
